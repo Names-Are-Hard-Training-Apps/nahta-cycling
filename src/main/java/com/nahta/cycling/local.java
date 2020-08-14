4f1080fc752087ea4cfcf10e71aacacbbb6db0fb
douglasawh <douglasawh@gmail.com>
2020-08-14 13:36:35 -0500

    This may serve as an example to anyone that wants to help out.

--- src/main/java/com/nahta/cycling/local.java
+++ src/main/java/com/nahta/cycling/local.java
@@ -1,5 +1,8 @@
 package com.nahta.cycling;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 //This is if the user wants to import a local file. We start with the Garmin tcx format.
 
 
@@ -13,7 +16,17 @@ public class local extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         // this obviously needs to not be a local folder eventually
-        // this is just a test to see if Camel works at all with the current import (no idea what I am doing)
-        from("file:/Users/dougwhitfield/Downloads/Personal?recursive=true&include=*.tcx").to("file:/Users/dougwhitfield/Downloads?recursive=true&include=*.tcx");
-    }
+        // this code works, but is not very useful.
+        from("file:/Users/dougwhitfield/Downloads/Personal?recursive=true&include=1590839323-90 min Power Zone Endurance Ride with Matt Wilpers-18f8eff79957451a8079d3cd2527db07.tcx").to("file:output?fileName=/Users/dougwhitfield/Downloads");
+    
+/*The file is not getting output in the two statement, so we aren't able to read it.
+          try (BufferedReader br = new BufferedReader(new FileReader("/Users/dougwhitfield/Downloads/90_min_Power_Zone_Endurance_Ride_with_Matt_Wilpers.tcx"))) {
+ 
+     	   String line;
+     	   while ((line = br.readLine()) != null) {
+     	       System.out.println(line);
+    
+    
+    	   } */
+    	}
 }
